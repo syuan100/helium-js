0e2004351dfd9e9ebbc7218d3e2f8a636017782e
Matt Reetz <matthewcarlreetz@gmail.com>
2021-08-06 08:25:00 -0500

    WIP

--- packages/http/src/resources/Transactions.ts
+++ packages/http/src/resources/Transactions.ts
@@ -1,3 +1,4 @@
+import camelcaseKeys from 'camelcase-keys'
 import type Client from '../Client'
 import type { AnyTransaction, TxnJsonObject } from '../models/Transaction'
 import Block from '../models/Block'
@@ -7,6 +8,40 @@ import PendingTransaction from '../models/PendingTransaction'
 import ResourceList from '../ResourceList'
 import Hotspot from '../models/Hotspot'
 import Validator from '../models/Validator'
+
+interface Counts {
+  varsV1: number;
+  validatorHeartbeatV1: number;
+  unstakeValidatorV1: number;
+  transferValidatorStakeV1: number;
+  transferHotspotV1: number;
+  tokenBurnV1: number;
+  tokenBurnExchangeRateV1: number;
+  stateChannelOpenV1: number;
+  stateChannelCloseV1: number;
+  stakeValidatorV1: number;
+  securityExchangeV1: number;
+  securityCoinbaseV1: number;
+  routingV1: number;
+  rewardsV2: number;
+  rewardsV1: number;
+  redeemHtlcV1: number;
+  priceOracleV1: number;
+  pocRequestV1: number;
+  pocReceiptsV1: number;
+  paymentV2: number;
+  paymentV1: number;
+  ouiV1: number;
+  genGatewayV1: number;
+  dcCoinbaseV1: number;
+  createHtlcV1: number;
+  consensusGroupV1: number;
+  consensusGroupFailureV1: number;
+  coinbaseV1: number;
+  assertLocationV2: number;
+  assertLocationV1: number;
+  addGatewayV1: number;
+}
 
 interface ListParams {
   cursor?: string
@@ -45,6 +80,17 @@ export default class Transactions {
     const url = `/transactions/${hash}`
     const { data: { data } } = await this.client.get(url)
     return Transaction.fromJsonObject(data)
+  }
+
+  async counts() {
+    let url = ''
+    if (this.context instanceof Validator) {
+      url = `/validators/${this.context.address}/activity/counts`
+    } else {
+      throw new Error('invalid context')
+    }
+    const { data: { data: stats } } = await this.client.get(url)
+    return camelcaseKeys(stats) as Counts
   }
 
   async list(params: ListParams = {}): Promise<ResourceList<AnyTransaction>> {
